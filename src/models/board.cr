825b50989764c2c5f1d74f6e8f949d05475b46c1
Andrew Ma <ama@bp-3.com>
2017-12-15 10:16:31 -0600

    initialize all pieces

--- src/models/board.cr
+++ src/models/board.cr
@@ -11,24 +11,47 @@ class Board < Granite::ORM::Base
   # id : Int64 primary key is created for you
   timestamps
 
+  @row_white = 0
+  @row_black = 7
+
   def initialize_pieces(game) : Nil
     self.save
     self.game = game 
     @title = "Game Board"
     
-    initialize_pawns(true)
-    initialize_pawns(false)
+    initialize_pawns()
+
+    initialize_pair_pieces("rook", [0, 7])
+    initialize_pair_pieces("knight", [1, 6])
+    initialize_pair_pieces("bishop", [2, 5])
+
+    initialize_power_pieces("king", 3)
+    initialize_power_pieces("queen", 4) 
   end
 
-  def initialize_pawns(color) : Nil
-    y = color ? 1 : 6
-    (0..8).each do |i|
-      new_pawn = Piece.new(name: "pawn", color: color)
-      new_pawn.x = i
-      new_pawn.y = y
-      new_pawn.board = self
-      new_pawn.save
+  def create_piece(name, color, x, y)
+    piece = Piece.new(name: name, color: color, x: x, y: y)
+    piece.board = self
+    piece.save
+  end
+
+  def initialize_pawns() : Nil
+    (0..7).each do |i|
+      create_piece("pawn", true, @row_white+1, i)
+      create_piece("pawn", false, @row_black-1, i)
     end
+  end
+
+  def initialize_pair_pieces(name : String, y_positions : Array(Int32)) : Nil
+    y_positions.each do |y|
+      create_piece(name, true, @row_white, y)
+      create_piece(name, false, @row_black, y)
+    end
+  end
+
+  def initialize_power_pieces(name : String, y_position : Int32) : Nil
+    create_piece(name, true, @row_white, y_position)
+    create_piece(name, false, @row_black, y_position)
   end
 
 end
